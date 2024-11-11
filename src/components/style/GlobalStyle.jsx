9c2306c0f293907e50fdbe748d1954f20b096516
yongZin-MAC <>
2024-11-11 22:01:31 +0900

    feat: Add reset style / fonts / favicon

---
+++ src/components/style/GlobalStyle.jsx
@@ -0,0 +1,74 @@
+import { createGlobalStyle } from "styled-components";
+import NanumSquareOTF_acL from "../fonts/NanumSquareOTF_acL.woff";
+import NanumSquareOTF_acR from "../fonts/NanumSquareOTF_acR.woff";
+import NanumSquareOTF_acB from "../fonts/NanumSquareOTF_acB.woff";
+import NanumSquareOTF_acEB from "../fonts/NanumSquareOTF_acEB.woff";
+
+const GlobalStyle = createGlobalStyle`
+  :root{
+    /* 폰트 변수 */
+    --f-light:"NanumSquareOTF_acL";
+    --f-reular:"NanumSquareOTF_acR";
+    --f-bold:"NanumSquareOTF_acB";
+    --f-ebold:"NanumSquareOTF_acEB";
+    /* //폰트 변수 */
+  }
+
+	*{
+    margin:0;
+    padding:0;
+    outline:none;
+    list-style:none;
+    box-sizing:border-box;
+  }
+
+  body{
+    margin:0;
+    padding:0;
+    line-height:normal;
+    font-family:var(--f-bold);
+    border:0;
+    overflow-x:hidden;
+  }
+
+	img{
+    width:100%;
+  }
+
+  button{
+    font-family:var(--f-bold);
+    border:0;
+    background-color:transparent;
+    cursor:pointer;
+  }
+
+  @font-face {
+    font-family:"NanumSquareOTF_acL";
+    src:url(${NanumSquareOTF_acL}) format("woff");
+    font-weight:normal;
+    font-style:normal;
+  }
+
+  @font-face {
+    font-family:"NanumSquareOTF_acR";
+    src:url(${NanumSquareOTF_acR}) format("woff");
+    font-weight:normal;
+    font-style:normal;
+  }
+
+  @font-face {
+    font-family:"NanumSquareOTF_acB";
+    src:url(${NanumSquareOTF_acB}) format("woff");
+    font-weight:normal;
+    font-style:normal;
+  }
+
+  @font-face {
+    font-family:"NanumSquareOTF_acEB";
+    src:url(${NanumSquareOTF_acEB}) format("woff");
+    font-weight:normal;
+    font-style:normal;
+  }
+`;
+
+export default GlobalStyle;
