e706366f1c2f677bd53bbe1aa61c689386c6df74
yongZin-MAC <>
2025-01-02 18:08:48 +0900

    feat: Add beforeunload TOP

--- src/components/context/SectionContext.jsx
+++ src/components/context/SectionContext.jsx
@@ -6,14 +6,28 @@ export const SectionContext = createContext();
 export const SectionProvider = ({ children }) => {
   const [sectionConfig, setSectionConfig] = useState([]);
   const [activeSection, setActiveSection] = useState("");
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [hideLoading, setHideLoading] = useState(false);
+
+  useEffect(() => { //로딩 애니메이션 종료
+    if(isLoaded) {
+      window.scrollTo(0, 1);
+
+      const timer = setTimeout(() => {
+        setHideLoading(true);
+      }, 2000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [isLoaded]);
 
   useEffect(() => {
-    // if (!sectionConfig) {
-    //   window.scrollTo(0, 0);
-    // }
-    const handleBeforeUnload = () => {
+    const handleBeforeUnload = () => { //새로고침 시 스크롤 최상단
       window.scrollTo(0, 0);
+      if(sectionConfig) setIsLoaded(true);
     }
+
+    if(sectionConfig.length > 0) setIsLoaded(true); //로드 완료시 로딩 종료
 
     window.addEventListener("beforeunload", handleBeforeUnload);
 
@@ -24,7 +38,9 @@ export const SectionProvider = ({ children }) => {
 
   const SectionContextValue = {
     sectionConfig, setSectionConfig,
-    activeSection, setActiveSection
+    activeSection, setActiveSection,
+    isLoaded, setIsLoaded,
+    hideLoading, setHideLoading
 	};
 
   return(
